70fab111bdf225c89bd91fe71e919f9b69784e0a
NatsubiSogan <61725249+NatsubiSogan@users.noreply.github.com>
2021-09-05 17:06:54 +0900

    Create z-algorithm.py

---
+++ z-algorithm.py
@@ -0,0 +1,20 @@
+# Z-Algorithm
+def z_algorithm(s: str) -> list:
+	str_len = len(s)
+	res = [0] * str_len
+	res[str_len - 1] = str_len
+	i, j = 1, 0
+	while i < str_len:
+		while i + j < str_len and s[i + j] == s[j]:
+			j += 1
+		res[i] = j
+		if j == 0:
+			i += 1
+			continue
+		k = 1
+		while i + k < str_len and j > res[k] + k:
+			res[i + k] = res[k]
+			k += 1
+		i += k
+		j -= k
+	return res

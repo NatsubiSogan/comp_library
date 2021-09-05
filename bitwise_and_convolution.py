fb8746ca1d7169263e5c6a7448e04511a8f3d76d
NatsubiSogan <61725249+NatsubiSogan@users.noreply.github.com>
2021-09-05 18:15:14 +0900

    Create bitwise_and/or_convolution.py

---
+++ bitwise_and_convolution.py
@@ -0,0 +1,34 @@
+# Bitwise And Convolution
+
+class BitwiseAndConvolution:
+	def __init__(self, mod: int = 998244353) -> None:
+		self.mod = mod
+
+	def fast_zeta_transform_and(self, a: list) -> list:
+		n = len(a)
+		b = (n - 1).bit_length()
+		for i in range(b):
+			bit = 1 << i
+			for j in range(n):
+				if not bit & j:
+					a[j] += a[bit | j]
+					a[j] %= self.mod
+		return a
+
+	def fast_mobius_transform_and(self, a: list) -> list:
+		n = len(a)
+		b = (n - 1).bit_length()
+		for i in range(b):
+			bit = 1 << i
+			for j in range(n):
+				if not bit & j:
+					a[j] -= a[bit | j]
+					a[j] %= self.mod
+		return a
+
+	def bitwise_and_convolution(self, a: list, b: list) -> list:
+		n = len(a)
+		A = self.fast_zeta_transform_and(a)
+		B = self.fast_zeta_transform_and(b)
+		C = [i * j % self.mod for i, j in zip(A, B)]
+		return self.fast_mobius_transform_and(C)
---
+++ bitwise_or_convolution.py
@@ -0,0 +1,34 @@
+# Bitwise Or Convolution
+
+class BitwiseOrConvolution:
+	def __init__(self, mod: int = 9888244353) -> None:
+		self.mod = mod
+
+	def fast_zeta_transform_or(self, a: list) -> list:
+		n = len(a)
+		b = (n - 1).bit_length()
+		for i in range(b):
+			bit = 1 << i
+			for j in range(n):
+				if bit & j:
+					a[j] += a[bit ^ j]
+					a[j] %= self.mod
+		return a
+
+	def fast_mobius_transform_or(self, a: list) -> list:
+		n = len(a)
+		b = (n - 1).bit_length()
+		for i in range(b):
+			bit = 1 << i
+			for j in range(n):
+				if bit & j:
+					a[j] -= a[bit ^ j]
+					a[j] %= self.mod
+		return a
+
+	def bitwise_or_convolution(self, a: list, b: list) -> list:
+		n = len(a)
+		A = self.fast_zeta_transform_or(a)
+		B = self.fast_zeta_transform_or(b)
+		C = [i * j % self.mod for i, j in zip(A, B)]
+		return self.fast_mobius_transform_or(C)

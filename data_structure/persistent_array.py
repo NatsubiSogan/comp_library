5fe67d22183dc073467c00189bfc2070452e880f
NatsubiSogan <61725249+NatsubiSogan@users.noreply.github.com>
2021-09-05 20:09:36 +0900

    Fixed persistent_array.py

--- data_structure/persistent_array.py
+++ data_structure/persistent_array.py
@@ -1,23 +1,42 @@
-import persistent_array
+# 永続配列
+class PersistentArrayNode:
+	def __init__(self, log: int) -> None:
+		self.val = None
+		self.ch = [None] * (1 << log)
 
-# 完全永続Union-Find
-class PersistentUnionFind:
-	def __init__(self, n: int) -> None:
-		self.par = persistent_array.PersistentArray()
-		self.rt = self.par.build([-1] * n)
-	
-	def find(self, x: int, t: int) -> int:
-		p = self.par.get(x, t)
-		if p < 0: return x
-		return self.find(p, t)
+class PersistentArray:
+	def __init__(self, log: int = 4) -> None:
+		self.log = log
+		self.mask = (1 << log) - 1
 
-	def unite(self, x: int, y: int, t: int) -> int:
-		p = self.find(x, t)
-		q = self.find(y, t)
-		if x == y: return t
-		px = self.par.get(p, t)
-		qy = self.par.get(q, t)
-		if px > qy:
-			p, q = q, p
-		tmp = self.par.set(q, p, t)
-		return self.par.set(p, px + qy, tmp)
+	def build(self, array: list) -> typing.Union[PersistentArrayNode, None]:
+		rt = None
+		for i, val in enumerate(array):
+			rt = self.init_set(i, val, rt)
+		return rt
+
+	def init_set(self, i: int, val: typing.Any, t: typing.Union[PersistentArrayNode, None]) -> PersistentArrayNode:
+		if t is None:
+			t = PersistentArrayNode(self.log)
+		if i == 0:
+			t.val = val
+		else:
+			t.ch[i & self.mask] = self.init_set(i >> self.log, val, t.ch[i & self.mask])
+		return t
+
+	def set(self, i: int, val: typing.Any, t: int) -> PersistentArrayNode:
+		res = PersistentArrayNode(self.log)
+		if t is not None:
+			res.ch = t.ch[:]
+			res.val = t.val
+		if i == 0:
+			res.val = val
+		else:
+			res.ch[i & self.mask] = self.set(i >> self.log, val, res.ch[i & self.mask])
+		return res
+		
+	def get(self, i: int, t: int) -> typing.Any:
+		if i == 0:
+			return t.val
+		else:
+			return self.get(i >> self.log, t.ch[i & self.mask])
